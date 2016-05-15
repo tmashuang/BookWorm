1a10236d6ef45ed65ce472396bbae5fa25df3400
tmashuang <tmashuang@gmail.com>
2016-05-14 17:08:04 -0700

    On tab creation feature working, WIP chain event with bookmark

--- bookWorm.js
+++ bookWorm.js
@@ -1,1 +1,9 @@
-console.log("I am here")
+$(document).ready(function() {
+  onCreatedChrome()
+});
+
+function onCreatedChrome() {
+  chrome.tabs.onCreated.addListener(function(tab) {
+    alert("hello");
+  });
+}
